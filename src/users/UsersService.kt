02525aee21683819830526c6318c58ac7556f6cb
Edem Topuzov <keltikan@gmail.com>
2021-06-16 23:42:55 +0300

    Add service to create the user

---
+++ src/users/UsersService.kt
@@ -0,0 +1,29 @@
+package com.mines.users
+
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+import com.mines.UnprocessableEntityError
+
+interface UsersService {
+    suspend fun create(email: String): User
+}
+
+class UsersServiceDB : UsersService {
+    override suspend fun create(email: String): User {
+        return transaction {
+            addLogger(StdOutSqlLogger)
+
+            if (Users.select { Users.email eq email }.firstOrNull() != null) throw UnprocessableEntityError("User exists")
+
+            val id = Users.insertAndGetId { s -> s[Users.email] = email }
+
+            Users.select { Users.id eq id }.first()
+        }.asUser()
+    }
+
+    private fun ResultRow.asUser() = User(
+        this[Users.id].value,
+        this[Users.email],
+        this[Users.isAdmin],
+    )
+}
