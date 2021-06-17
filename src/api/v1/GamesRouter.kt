36519e68192ea4caa7a930cf4e77ae7f2acea2fc
Edem Topuzov <keltikan@gmail.com>
2021-06-17 22:59:19 +0300

    Add route to find game by id

--- src/api/v1/GamesRouter.kt
+++ src/api/v1/GamesRouter.kt
@@ -2,6 +2,7 @@ package com.mines.api.v1
 
 import com.mines.games.GamesServiceDB
 import io.ktor.application.*
+import io.ktor.features.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
@@ -9,6 +10,18 @@ fun Route.gamesRouter(gamesService: GamesServiceDB) {
     route("/api/v1/games") {
         post {
             val game = gamesService.create()
+            call.respond(game)
+        }
+
+        get("/{id}") {
+            val id =
+                try {
+                    requireNotNull(call.parameters["id"]).toInt()
+                } catch (e: NumberFormatException) {
+                    throw NotFoundException()
+                }
+
+            val game = gamesService.findById(id)
             call.respond(game)
         }
     }
