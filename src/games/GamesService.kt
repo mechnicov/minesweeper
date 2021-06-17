eb2777ac39ac74c6d7869c9ea28c78ddf0acdff5
Edem Topuzov <keltikan@gmail.com>
2021-06-18 02:52:56 +0300

    Add service to mark the cell

--- src/games/GamesService.kt
+++ src/games/GamesService.kt
@@ -3,6 +3,7 @@ package com.mines.games
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 import com.mines.cells.Cell
+import com.mines.cells.CellStatus
 import com.mines.settings.Setting
 import com.mines.users.User
 import io.ktor.features.*
@@ -12,6 +13,7 @@ interface GamesService {
     suspend fun create(): GameData
     suspend fun findById(id: Int): GameData
     suspend fun all(): List<GameData>
+    suspend fun markCell(gameId: Int, x: Int, y: Int): GameData
 }
 
 class GamesServiceDB : GamesService {
@@ -63,6 +65,27 @@ class GamesServiceDB : GamesService {
     override suspend fun all(): List<GameData> {
         return transaction {
             Game.all().map { it.data() }
+        }
+    }
+
+    override suspend fun markCell(gameId: Int, x: Int, y: Int): GameData {
+        return transaction {
+            addLogger(StdOutSqlLogger)
+
+            val game = Game.findById(gameId)
+
+            game ?: throw NotFoundException()
+
+            val cell = game.cells.find { it.x == x && it.y == y }
+
+            cell ?: throw NotFoundException()
+
+            when (cell.status) {
+                CellStatus.CLOSED -> cell.status = CellStatus.MARKED
+                CellStatus.MARKED -> cell.status = CellStatus.CLOSED
+            }
+
+            game.data()
         }
     }
 }
