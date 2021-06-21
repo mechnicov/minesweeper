a768cb126b16a4475c7083c28cf35e6c95d5bfb0
Edem Topuzov <keltikan@gmail.com>
2021-06-21 14:34:00 +0300

    Add alert reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
+import alertReducer from './alertReducer'
 import authReducer from './authReducer'
 
 export default combineReducers({
+  alertReducer,
   authReducer,
 })
