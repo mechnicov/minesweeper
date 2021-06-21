22ddbf8aa49232bbbce0fcac861227c135fef066
Edem Topuzov <keltikan@gmail.com>
2021-06-21 18:20:30 +0300

    Change header content due to user role

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,29 +1,41 @@
 import React, { Fragment } from 'react'
+import { useHistory } from 'react-router'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { Navbar, Nav } from 'react-bootstrap'
 
 import { logout } from '../../actions/authActions'
 
-const Header = ({ logout, isLoggedIn }) => {
+const Header = ({ logout, isLoggedIn, user }) => {
+  let history = useHistory()
+
+  const logOutAndRedirect = () => {
+    logout()
+    history.push('/')
+  }
+
   const leftGuestLinks = (
     <Fragment>
-      <Nav.Link href='/sign_in'>Войти</Nav.Link>
-      <Nav.Link href='/sign_up'>Зарегистрироваться</Nav.Link>
+      <Nav.Link href='/sign_in'>Sign in</Nav.Link>
+      <Nav.Link href='/sign_up'>Sign up</Nav.Link>
     </Fragment>
   )
 
   const leftUserLinks = (
     <Fragment>
-      <Nav.Link href='#'>Новая игра</Nav.Link>
-      <Nav.Link href='/games'>Список игр</Nav.Link>
+      <Nav.Link href='#'>New game</Nav.Link>
+      <Nav.Link href='/games'>My games</Nav.Link>
     </Fragment>
   )
 
-  const logoutLink = (
-    <Nav>
-      <Nav.Link href='#!' onClick={logout}>Выход</Nav.Link>
-    </Nav>
+  const rightUserLinks = (
+    <Fragment>
+      <Nav>
+        <Nav.Link href='#!'>{user && user.email}</Nav.Link>
+        {user && user.isAdmin && <Nav.Link href='#!'>Settings</Nav.Link>}
+        <Nav.Link href='#!' onClick={logOutAndRedirect}>Log out</Nav.Link>
+      </Nav>
+    </Fragment>
   )
 
   return (
@@ -33,19 +45,21 @@ const Header = ({ logout, isLoggedIn }) => {
           {isLoggedIn ? leftUserLinks : leftGuestLinks}
         </Nav>
 
-        {isLoggedIn && logoutLink}
+        {isLoggedIn && rightUserLinks}
       </Navbar.Collapse>
     </Navbar>
   )
 }
 
 Header.propTypes = {
+  user: PropTypes.object,
   logout: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool,
+  isLoggedIn: PropTypes.bool.isRequired,
 }
 
 const mapStateToProps = state => ({
   isLoggedIn: state.authReducer.isLoggedIn,
+  user: state.authReducer.user,
 })
 
 export default connect(mapStateToProps, { logout })(Header)
