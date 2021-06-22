0c9a4f4b57fb5102507d582801c8c960c52a263f
Edem Topuzov <keltikan@gmail.com>
2021-06-22 14:59:42 +0300

    Add get current game action

---
+++ client/src/actions/gameActions.js
@@ -0,0 +1,30 @@
+import {
+  GET_GAME,
+  OPEN_CELL,
+  MARK_CELL,
+} from './types'
+
+import { doGetOneGame, doMarkCell, doOpenCell } from '../utils/gameServices'
+import { setAlert } from './alertActions'
+
+export const getOneGame = gameId => dispatch => {
+  return doGetOneGame(gameId).
+    then(response => {
+      dispatch({
+        type: GET_GAME,
+        payload: response.data
+      })
+
+      return Promise.resolve()
+    },
+    error => {
+      const msg = (error.response && error.response.data && error.response.data.message) ||
+                  error.message ||
+                  error.toString()
+
+      dispatch(setAlert(msg))
+
+      return Promise.resolve()
+    }
+  )
+}
