e7af1b193abbe95993d3d5ea545b4f526a1157ee
Edem Topuzov <keltikan@gmail.com>
2021-06-22 14:58:57 +0300

    Add game API request services

--- client/src/utils/gameServices.js
+++ client/src/utils/gameServices.js
@@ -1,7 +1,19 @@
 import axios from 'axios'
 
-import authHeader from './authHeader'
+import { authHeader } from './authHeader'
 
-export const getAllGames = () => {
+export const doGetAllGames = () => {
   return axios.get('/api/v1/games', { headers: authHeader() })
+}
+
+export const doGetOneGame = gameId => {
+  return axios.get(`/api/v1/games/${gameId}`, { headers: authHeader() })
+}
+
+export const doMarkCell = (gameId, x, y) => {
+  return axios.post(`/api/v1/games/${gameId}/mark`, { x: x, y: y } , { headers: authHeader() })
+}
+
+export const doOpenCell = (gameId, x, y) => {
+  return axios.post(`/api/v1/games/${gameId}/open`, { x: x, y: y } , { headers: authHeader() })
 }
