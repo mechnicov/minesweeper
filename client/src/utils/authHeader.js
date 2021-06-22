36d3dd7f6ef72b4c79a99ff5fd17d2bda2f2f3e7
Edem Topuzov <keltikan@gmail.com>
2021-06-22 14:57:27 +0300

    Update request headers service

--- client/src/utils/authHeader.js
+++ client/src/utils/authHeader.js
@@ -1,9 +1,11 @@
 export const authHeader = () => {
+  let headers = { 'Content-Type': 'application/json' }
+
   const minesweeper = JSON.parse(localStorage.getItem('minesweeper'))
 
   if (minesweeper && minesweeper.token) {
-    return { 'Authorization': `Bearer ${minesweeper.token}` }
-  } else {
-    return {}
+    headers = { ...headers, 'Authorization': `Bearer ${minesweeper.token}` }
   }
+
+  return headers
 }
